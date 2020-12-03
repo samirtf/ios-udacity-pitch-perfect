2a3a26df72e1783b16772f5e76e9d487aee38899
Virtus JFL <virtusjfl@Mac-mini-de-Virtus.local>
2020-12-03 19:35:45 -0300

    settings: initial project

---
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Virtus JFL on 03/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
